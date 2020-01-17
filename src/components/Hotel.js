80922c877ef6fbfdc926e33ee9d115b8795edf9a
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-17 17:41:03 +0530

    Update Hotel.js

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,38 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import HotelItem from './HotelItem';
 
 export class Hotel extends Component {
     render() {
-        return (
-            <div>
-                <h1>Hotel App</h1>
-                <div style={styleHotel}>
-                    <p>Name: {this.props.hotels[0].name}</p>
-                    <p>Location: {this.props.hotels[0].location}</p>
-                    <p>Rating: {this.props.hotels[0].rating}</p>
-                </div>
-                <div style={styleHotel}>
-                    <p>Name: {this.props.hotels[1].name}</p>
-                    <p>Location: {this.props.hotels[1].location}</p>
-                    <p>Rating: {this.props.hotels[1].rating}</p>
-                </div>
-                <div style={styleHotel}>
-                    <p>Name: {this.props.hotels[2].name}</p>
-                    <p>Location: {this.props.hotels[2].location}</p>
-                    <p>Rating: {this.props.hotels[2].rating}</p>
-                </div>
-            </div>
-        )
+        return this.props.hotels.map((hotel) => (
+            <HotelItem hotel={hotel}/>
+        ))
     }
 }
 
-const styleHotel = {
-    display : "inline-block",
-    margin : "10px",
-    border : "2px solid #eee",
-    boxShadow : "0 2px 2px #ccc", 
-    background : "white",
-    width : "200px",
-    padding : "20px",
-}
 
 export default Hotel
