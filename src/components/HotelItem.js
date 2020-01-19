d0e7fe02b6eec2473af2fc0f63e3a22f26f8cb3a
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-19 15:36:50 +0530

    Update HotelItem.js

--- src/components/HotelItem.js
+++ src/components/HotelItem.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import Forest from './../forest.jpg';
 
+let change = false;
+
 export class HotelItem extends Component {
+
+    setChange = () => {
+        change = !change;
+        console.log(change);
+    }
+
     render() {
         return (
             <div style={styleMain}>
-            <div style={styleHotel}>
-                <p>Name: {this.props.hotel.name}</p>
-                <p>Location: {this.props.hotel.location}</p>
-                <p>Rating: {this.props.hotel.rating}</p>
-                <img src = {Forest}/>
-            </div>
+                <div style={styleHotel} onClick= {this.setChange}>
+                    <p>Name: {this.props.hotel.name}</p>
+                    <p>Location: {this.props.hotel.location}</p>
+                    <p>Rating: {this.props.hotel.rating}</p>
+                    <img src = {Forest} alt="A Forest"/>
+                </div>
             </div>
         )
     }
@@ -26,7 +34,7 @@ const styleHotel = {
     margin : "10px",
     border : "2px solid #eee",
     boxShadow : "0 2px 2px #ccc", 
-    background : "white",
+    background : ({change} === true)?"red":"white",
     width : "200px",
     height : "300px",
     padding : "20px",
