58f3ce25719b08a8d703648c25e8b867407b0909
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-21 10:13:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,27 +10,39 @@ export class App extends Component {
         name : "Hotel 1",
         location : "Guwahati",
         rating : "4.4",
+        selected : false,
       },
       {
         id : 2,
         name : "Hotel 2",
         location : "Kolkata",
         rating : "4.2",
+        selected : false,
       },
       {
         id : 3,
         name : "Hotel 3",
         location : "Saltlake",
         rating : "4.3",
+        selected : false,
       }
     ]
+  }
+
+  toggleSelected = (id) => {
+    this.setState({ hotels : this.state.hotels.map((hotel) => {
+      if(hotel.id === id){
+        hotel.completed = !hotel.completed
+      }
+      return hotel;
+    }) });
   }
   
   render() {
     return (
       <div>
         <h1 style= {{textAlign : "center"}}>Hotel App</h1>
-        <Hotel hotels={this.state.hotels}/>
+        <Hotel hotels={this.state.hotels} toggleSelected={this.toggleSelected}/>
       </div>
     )
   }
