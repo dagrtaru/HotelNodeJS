a262aba596a675ca149f3282006fb57dc72a21d9
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-21 10:13:43 +0530

    Update Hotel.js

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -4,7 +4,7 @@ import HotelItem from './HotelItem';
 export class Hotel extends Component {
     render() {
         return this.props.hotels.map((hotel) => (
-            <HotelItem hotel={hotel}/>
+            <HotelItem hotel={hotel} toggleSelected={this.props.toggleSelected}/>
         ))
     }
 }
