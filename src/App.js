d8dbc8b1d2bf2ed82ba96e38a261e21584b4b27f
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-26 16:27:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Hotel from './components/Hotel';
+import AddHotel from './components/AddHotel';
 
 export class App extends Component {
   
@@ -9,6 +10,7 @@ export class App extends Component {
         id : 1,
         name : "Hotel 1",
         location : "Guwahati",
+        story : "Guwahati  is the largest city in the Northeastern Indian state of Assam and also the largest metropolis in the northeastern India. A major riverine port city along with hills is one of the fastest growing cities in India, Guwahati is situated on the south bank of the Brahmaputra",
         rating : "4.4",
         selected : false,
       },
@@ -16,6 +18,7 @@ export class App extends Component {
         id : 2,
         name : "Hotel 2",
         location : "Kolkata",
+        story : "Kolkata , also known as Calcutta  (the official name until 2001) is the capital of the Indian state of West Bengal. According to the 2011 Indian census, it is the seventh most populous city; the city had a population of 4.5 million, while the suburb population brought the total to 14.1 million",
         rating : "4.2",
         selected : false,
       },
@@ -23,6 +26,7 @@ export class App extends Component {
         id : 3,
         name : "Hotel 3",
         location : "Saltlake",
+        story : "Salt Lake is a city in Bidhannagar Municipal Corporation of North 24 Parganas district in the Indian state of West Bengal. It is close to Kolkata and also a part of the area covered by Kolkata Metropolitan Development Authority (KMDA). It was developed between 1958 and 1965 as a planned satellite city",
         rating : "4.3",
         selected : false,
       }
@@ -41,7 +45,8 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <h1 style= {{textAlign : "center"}}>Hotel App</h1>
+        <h1 style= {{fontFamily : "arial", textAlign : "center"}}>Hotel App</h1>
+        <AddHotel />
         <Hotel hotels={this.state.hotels} toggleSelected={this.toggleSelected}/>
       </div>
     )
