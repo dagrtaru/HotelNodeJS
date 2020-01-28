1b89f060a0dabbdd37a2d542bfee03bacee6ff24
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-28 09:54:42 +0530

    Update AddHotel.js

--- src/components/AddHotel.js
+++ src/components/AddHotel.js
@@ -6,13 +6,19 @@ export class AddHotel extends Component {
         title : ""
     }
 
-    onChange = (e) =>{
-        this.setState({title : e.target.value});
+    onSubmit = (e) => {
+        this.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title : ""});
+    }
+
+    onChange = (e) => {
+        e.setState({title : e.target.value});
     }
 
     render() {
         return (
-            <form style={{ display: 'flex' }}>
+            <form style={{ display: 'flex' }} onSubmit={this.onSubmit}>
                 <input type="text" name="title" style={{flex : "10", padding : "5px"}} 
                 placeholder="Add Hotel..." value={this.state.title} onChange={this.onChange}/>
                 <input type="submit" value="Submit" className="btn" style={{flex : "1"}} />
