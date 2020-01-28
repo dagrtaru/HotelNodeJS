757204fdfd8da75ce71b7ef7e6d5f1b48aa89b77
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-28 09:10:11 +0530

    Added a state

--- src/components/AddHotel.js
+++ src/components/AddHotel.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 export class AddHotel extends Component {
+
+    state = {
+        title : ""
+    }
+
     render() {
         return (
             <form style={{ display: 'flex' }}>
-                <input type="text" name="title" style={{flex : "10", padding : "5px"}} placeholder="Add Hotel..."/>
+                <input type="text" name="title" style={{flex : "10", padding : "5px"}} 
+                placeholder="Add Hotel..." value={this.state.title} onChange={this.onChange}/>
                 <input type="submit" value="Submit" className="btn" style={{flex : "1"}} />
             </form>
         )
